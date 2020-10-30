c2ab6b13adad6f127f45ed8b19c2abb69f6bc00f
David Sterry <sterry.david@gmail.com>
2020-10-30 09:22:13 -0700

    start BI logic and  confirm form is working before starting to test

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 //User-Interface logic
 $(document).ready(function() {
-  $(userInput).submit(function(event) {
+  $("#userInput").submit(function(event) {
     event.preventDefault();
     const number = parseInt($("#anyNumber").val());
+    console.log(number);
     const boop = beepBoop(number);
 
     var results= boop.join(',');
@@ -13,4 +14,11 @@ $(document).ready(function() {
 
 
 //Business Interface Logic
-function beepBoop(number);
+function beepBoop(number) {
+  let resultsArray = [];
+  for (let i=0; i = number; i++) {
+    resultsArray.push([i]);
+      if (resultsArray[i]){}
+  }
+}
+
